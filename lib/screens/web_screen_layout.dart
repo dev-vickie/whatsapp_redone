4c74c682b484224a74858bc415b6bef8b01fbfb6
dev-vickie <vmutethia84@gmail.com>
2023-03-30 20:48:33 +0300

    show contacts on web

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_redone/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("This is web"),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [ContactsList()],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/backgroundImage.png'),
+                  fit: BoxFit.cover),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -10,48 +10,47 @@ class ContactsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: info.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(info[index]['profilePic'].toString()),
-                      ),
-                      title: Text(
-                        info[index]['name'].toString(),
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(top: 6),
-                        child: Text(info[index]['message'].toString()),
-                      ),
-                      trailing: Text(
-                        info[index]['time'].toString(),
-                        style: const TextStyle(
-                          fontSize: 13,
-                          color: Colors.grey,
-                        ),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(info[index]['profilePic'].toString()),
+                      radius: 30,
+                    ),
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(info[index]['message'].toString()),
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
                       ),
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-          const Divider(
-            color: dividerColor,
-            indent: 80,
-          )
-        ],
+                ),
+              ),
+              const Divider(
+                color: dividerColor,
+                indent: 80,
+              )
+            ],
+          );
+        },
       ),
     );
   }
